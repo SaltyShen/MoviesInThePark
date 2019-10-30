7e703fc0619d69ec863af92b74b4fb84b1d5108f
SaltyShen <47713674+SaltyShen@users.noreply.github.com>
2019-10-30 03:32:56 -0500

    maybe this will work

--- js/moviesdatabase.js
+++ js/moviesdatabase.js
@@ -1,3 +1,19 @@
-$.getJSON = ("https://data.cityofchicago.org/resource/7piw-z6r6.json?date=2019-06-07T00:00:00.000", function( data ) {
-  console.log(data);
-});
+function ValidationEvent() {
+
+    park = document.getElementById("park").value;
+    title = document.getElementById("title").value;
+    day = document.getElementById("day").value;
+
+    $.getJSON = ("https://data.cityofchicago.org/resource/7piw-z6r6.json", function(park, title, day) {
+        console.log(data);
+        var queries = '';
+        $.each(data, function(key, value){
+            queries += '<tr>';
+            queries += '<td>'+value.park+'<td>';
+            queries += '<td>'+value.title+'<td>';
+            queries += '<td>'+value.day+'<td>';
+            queries += '</tr>'
+        });
+        $('#movietable').append(queries);
+    });
+}
