0667841dbd2da89093df15dc805820e9bbf5e6cd
SaltyShen <47713674+SaltyShen@users.noreply.github.com>
2019-10-30 04:20:36 -0500

    some errors

--- js/moviesdatabase.js
+++ js/moviesdatabase.js
@@ -1,19 +1,21 @@
 function ValidationEvent() {
 
+    /*
     park = document.getElementById("park").value;
     title = document.getElementById("title").value;
-    day = document.getElementById("day").value;
-
-    $.getJSON = ("https://data.cityofchicago.org/resource/7piw-z6r6.json", function(park, title, day) {
-        console.log(data);
+    day = document.getElementById("day").value;     */
+$(document).ready(function(){
+    $.getJSON = ("https://data.cityofchicago.org/resource/7piw-z6r6.json", function(data) {
+        console.log();
         var queries = '';
         $.each(data, function(key, value){
             queries += '<tr>';
             queries += '<td>'+value.park+'<td>';
             queries += '<td>'+value.title+'<td>';
             queries += '<td>'+value.day+'<td>';
-            queries += '</tr>'
+        
         });
-        $('#movietable').append(queries);
+        $('#movietable').append(value);
     });
+});
 }
