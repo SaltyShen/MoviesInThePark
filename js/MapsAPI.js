78bedace085f9f0d0d88e002334ea3f461fbd261
SaltyShen <47713674+SaltyShen@users.noreply.github.com>
2019-10-29 23:50:15 -0500

    updated and linked maps - fixed html erros

---
+++ js/MapsAPI.js
@@ -0,0 +1,1 @@
+        $http.get('https://maps.googleapis.com/maps/api/geocode/json?latlng=40.714224,-73.961452&key=AIzaSyC7MinVe2U_cNHvcun30I1EDEIUme14pv4')
--- js/moviesdatabase.js
+++ js/moviesdatabase.js
@@ -1,10 +1,3 @@
-'use strict';
-angular.module('Chicago Movies at The Park App',[])
-.controller('MovieSearch', ['$scope','$http', function($scope, $http){
-
-    $http.get('https://data.cityofchicago.org/resource/7piw-z6r6.json').then(function(response){
-    console.log(response.data);
-    $scope.moviesdatabase = response.data;
-    
-    });
-}]);
+$.getJSON = ("https://data.cityofchicago.org/resource/7piw-z6r6.json", function( data ) {
+  console.log(data);
+});
