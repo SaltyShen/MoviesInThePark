a4bbb55985158ef2ae3f54f0c371b867e4d0f82e
SaltyShen <47713674+SaltyShen@users.noreply.github.com>
2019-10-30 02:45:31 -0500

    try something again

--- js/moviesdatabase.js
+++ js/moviesdatabase.js
@@ -1,3 +1,3 @@
-$.getJSON = ("https://data.cityofchicago.org/resource/7piw-z6r6.json", function( data ) {
+$.getJSON = ("https://data.cityofchicago.org/resource/7piw-z6r6.json?date=2019-06-07T00:00:00.000", function( data ) {
   console.log(data);
 });
