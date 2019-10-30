d99c9866b8dad2c755145d5f3f1d5bde63f371be
SaltyShen <47713674+SaltyShen@users.noreply.github.com>
2019-10-30 09:57:17 -0500

    javascript now works. Index still needs tweaks. No Map API so far.

--- js/moviesdatabase.js
+++ js/moviesdatabase.js
@@ -1,21 +1,28 @@
 function ValidationEvent() {
-
-    /*
+    
     park = document.getElementById("park").value;
     title = document.getElementById("title").value;
-    day = document.getElementById("day").value;     */
-$(document).ready(function(){
-    $.getJSON = ("https://data.cityofchicago.org/resource/7piw-z6r6.json", function(data) {
-        console.log();
-        var queries = '';
-        $.each(data, function(key, value){
-            queries += '<tr>';
-            queries += '<td>'+value.park+'<td>';
-            queries += '<td>'+value.title+'<td>';
-            queries += '<td>'+value.day+'<td>';
-        
-        });
-        $('#movietable').append(value);
+    day = document.getElementById("day").value;
+    limiter = $("#upcomingonly").attr("checked"); /*1 : 0*/
+    var date=new Date();
+    var today=new Date(date.getTime() - (date.getTimezoneOffset() * 60000)).toISOString().replace(/T/, ' ').replace(/\..+/, ''); 
+
+
+    $.ajax({
+        url: "https://data.cityofchicago.org/resource/7piw-z6r6.json",
+        type: "GET",
+        data: {
+        "$limit" : 5000,
+        "$$app_token" : "uLSbrg4uzRCHZyb0hogPfnljL",
+        "day" :  day,
+        "title" : title,
+        "park" : park
+        }
+
+    }).done(function(data) {
+    alert("Retrieved " + data.length + " records from the dataset!");
+    console.log(data);
+    $("#movietable").append(data);
     });
-});
+
 }
