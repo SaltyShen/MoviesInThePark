2cc76c8c5d285cfcf65c05a39d19800f41a8eb05
Shen Wang <peterhuiwang1995@gmail.com>
2020-04-03 00:58:32 -0500

    Added table to search tab. RM the query tab in favor of ease of use. Added data appending functionality to moviedatabase.js that will dynamically add queries to the table.

--- js/moviesdatabase.js
+++ js/moviesdatabase.js
@@ -6,41 +6,64 @@
 */
 
 function ValidationEvent() {
-    
-    // names returns node list - get[0] element for actual value
-    park = document.getElementsByName("park"); park = park[0].value;
-    title = document.getElementsByName("title"); title = title[0].value;
-    day = document.getElementsByName("day"); day = day[0].value;
 
-    if(park == null){console.log("park is null");}
+    var park = document.getElementById("park").value;
+    var title = document.getElementById("title").value;
+    var day = document.getElementById("day").value;
 
     limiter = $("#upcomingonly").attr("checked"); /*1 : 0*/
     var date=new Date();
     var today=new Date(date.getTime() - (date.getTimezoneOffset() * 60000)).toISOString().replace(/T/, ' ').replace(/\..+/, ''); 
 
-    
+
         $.ajax({
             url: "https://data.cityofchicago.org/resource/7piw-z6r6.json",
             type: "GET",
             data: {
 
                 "$limit" : 5000,
-                "$$app_token" : "uLSbrg4uzRCHZyb0hogPfnljL"
-                //"day" :  day,
-                //"title" : title,
-                //"park" : park
+                "$$app_token" : "uLSbrg4uzRCHZyb0hogPfnljL",
+                "day" :  day,
+                "title" : title,
+                "park" : park
 
-            },
-            success: function(data){
+            }
+            //success and error method depreciated.
+            //ajax 1.8 uses .done and .fail down below.
+            
+            /*success: function(data){
                 alert("Request Successful! \nFound: " + data.length + " entry points.");
+                //insertData(data);
             },
             error: function(){
                 alert("No values found.");
-            }
+            }*/
 
-        }).done(function(data) {
-        console.log(data);
-        $("#movietable").append(data);
-        });
+        }).done(function(data){
+            alert("Request Successful! \nFound: " + data.length + " entry points.");
+            let table = document.getElementById("movieTable");
+            let tbody = table.getElementsByTagName('tbody');
+            data.forEach(entry => {
+            //dynamically add to table.
+            let newRow = table.insertRow();
+            let newCell = newRow.insertCell(0);
+            let newText = '<tr><td>${entry[0]}</td> <td>${entry[1]}</td> <td>${entry[0]}</td></tr>';
+            newCell.append(newText);
+            })
 
+        })
+        .fail(function(){ alert("API Call failed: No data returned.")});
 }
+/*function insertData(data){
+
+    console.log(table.values());
+    //let tbody = table.getElementByTagName('tbody');
+
+    /*data.forEach(entry => {
+        var newRow = tbody.insertRow(table.rows.length-1);
+        var newCell = newRow.insertCell(newRow.cells.length);
+        newCell.innerHTML = "Something";
+    }*/
+
+
+
