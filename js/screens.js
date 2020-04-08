def72c8819be3b33355e7b4b3efa92ee04372443
Shen Wang <peterhuiwang1995@gmail.com>
2020-04-08 13:37:55 -0500

    final push in vanilla javascript. minor remodeling of css

---
+++ js/screens.js
@@ -0,0 +1,17 @@
+function hideScreens() {
+    $(".content").hide();
+  }
+
+
+  $(document).ready(function() {
+
+
+
+    $(".nav-link").on("click", function(){
+      hideScreens();
+      var target = $(this).attr("href");
+      $(target).show();
+      $(target).load("page_content/_" + target.replace("#","") + ".html");
+    });
+
+  });
